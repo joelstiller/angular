f89371bb148aa3fc692e2121900314934907cfe2
Joel Stiller <joelstiller@gmail.com>
2018-05-08 03:44:10 -0500

    Fixed a couple errors

--- squishytomato/src/app/newreview/newreview.component.ts
+++ squishytomato/src/app/newreview/newreview.component.ts
@@ -4,6 +4,8 @@ import { NgForm } from '@angular/forms';
 import { Review } from '../review';
 import { ReviewService } from '../services/review.service';
 import { ActivatedRoute } from '@angular/router';
+import { Movie } from '../movie';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-newreview',
@@ -12,6 +14,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class NewreviewComponent implements OnInit {
   review: Review = new Review();
+  movie: Movie = new Movie();
   @Output() createReview = new EventEmitter<Review>();
 
   id: string;
@@ -20,7 +23,8 @@ export class NewreviewComponent implements OnInit {
   constructor(
     public router: Router,
     private activatedRoute: ActivatedRoute,
-    private reviewService: ReviewService
+    private reviewService: ReviewService,
+    private movieService: MovieService
   ) {}
 
   ngOnInit() {
@@ -29,6 +33,9 @@ export class NewreviewComponent implements OnInit {
     });
     //  console.log(this.id);
     this.passid = this.id;
+    this.movieService.getMovie(this.passid).subscribe(movie => {
+      this.movie = movie;
+    });
   }
   onSubmit(event: Event, form: NgForm) {
     event.preventDefault();
